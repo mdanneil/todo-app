68936ce47fbbbdd43781385c9073e5e740d71257
ddewe <ddewee92@gmail.com>
2021-09-07 11:35:58 +0200

    added checklist component

---
+++ src/components/Checklist.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Checklist() {
+    return (
+        <div className="checkboxes">
+            <input type="checkbox" />
+            <p>Place text here</p>
+
+            <input type="checkbox" />
+            <p>Place text here</p>
+
+            <input type="checkbox" />
+            <p>Place text here</p>
+
+            <input type="checkbox" />
+            <p>Place text here</p>
+        </div>
+    );
+}
+
+export default Checklist;
