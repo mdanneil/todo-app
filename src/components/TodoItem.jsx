e8d33792a1072ad4fff99bae0890bfb9e0112bc9
ddewe <ddewee92@gmail.com>
2021-09-11 13:49:18 +0200

    got rid of checked warning

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -4,7 +4,11 @@ import todoItemList from "../todoItemList";
 function TodoItem(props) {
     return (
         <div className="single-box">
-            <input type="checkbox" checked={props.completed} />
+            <input
+                type="checkbox"
+                checked={props.completed}
+                onChange={() => console.log("Box checked")}
+            />
             <p>{props.text}</p>
         </div>
     );
